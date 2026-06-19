680fcedff272a5d9b0d859b5fdf567f81edb42fd
sayyidahmedubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2026-06-20 03:04:56 +0700

    clean up debug routes

--- routes/web.php
+++ routes/web.php
@@ -3,31 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\SongRequestController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', [PublicController::class, 'home']);
-Route::get('/', function () {
-    return 'ROOT OK';
-});
 
-
-
-
-///debug percobaan
-    Route::get('/debug', function () {
-    return 'debug-ok';
-});
 Route::get('/request', [SongRequestController::class, 'create'])
     ->name('request.create');
-
 Route::post('/request', [SongRequestController::class, 'store'])
     ->name('request.store');
 Route::get('/queue', [SongRequestController::class, 'queue'])
     ->name('queue');
 Route::get('/display', [SongRequestController::class, 'display'])
     ->name('display');
-
-    use App\Http\Controllers\AuthController;
-    use App\Http\Controllers\AdminController;
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.attempt');
@@ -37,16 +25,4 @@ Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/requests', [AdminController::class, 'requests'])->name('admin.requests');
     Route::patch('/requests/{songRequest}/status', [AdminController::class, 'updateStatus'])->name('admin.requests.updateStatus');
-});
-
-///cek php
-Route::get('/php-version', function () {
-    return [
-        'php_version' => PHP_VERSION,
-    ];
-});
-
-///TEST ROOT 
-Route::get('/', function () {
-    return 'ROOT WORKS';
 });
