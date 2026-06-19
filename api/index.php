3500caa9b64baf3254be640aa573cc22b18a2f48
sayyidahmedubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2026-06-20 01:31:00 +0700

    coba api lagi

--- api/index.php
+++ api/index.php
@@ -4,4 +4,12 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo "Autoload OK<br>";
+
+$app = require __DIR__ . '/../bootstrap/app.php';
+
+echo "App OK<br>";
