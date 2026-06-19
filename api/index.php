e17e88dd5ecfc8633df3f3dbf849b1fc869a1d35
sayyidahmedubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2026-06-20 01:20:19 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "API START<br>";
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-require __DIR__.'/../public/index.php';
+require __DIR__ . '/../public/index.php';
