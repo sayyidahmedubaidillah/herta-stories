8321c594894b4450ae4e566dcd1789fcb5de9efa
sayyidahmedubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2026-06-20 01:36:58 +0700

    belum ketemu

--- api/index.php
+++ api/index.php
@@ -14,8 +14,12 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $request = Illuminate\Http\Request::capture();
 
-$response = $kernel->handle($request);
-
-echo "Response OK<br>";
-
-$response->send();
+try {
+    $response = $kernel->handle($request);
+    echo "Response OK<br>";
+    $response->send();
+} catch (\Throwable $e) {
+    echo "Error: " . $e->getMessage() . "<br>";
+    echo "File: " . $e->getFile() . "<br>";
+    echo "Line: " . $e->getLine() . "<br>";
+}
