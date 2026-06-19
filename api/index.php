0a998d89013ab525f11fff689a7c0eafd5e0d638
sayyidahmedubaidillah <sayyidahmedubaidillah@saintek.unipdu.ac.id>
2026-06-20 01:46:30 +0700

    wkwk

--- api/index.php
+++ api/index.php
@@ -14,15 +14,8 @@ $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $request = Illuminate\Http\Request::capture();
 
-try {
-    $response = $kernel->handle($request);
-    echo "Response OK<br>";
-    $response->send();
-} catch (\Throwable $e) {
-    echo "Error: " . $e->getMessage() . "<br>";
-    echo "File: " . $e->getFile() . "<br>";
-    echo "Line: " . $e->getLine() . "<br>";
-    echo "Previous: " . ($e->getPrevious() ? $e->getPrevious()->getMessage() : 'none') . "<br>";
-    echo "Previous file: " . ($e->getPrevious() ? $e->getPrevious()->getFile() : 'none') . "<br>";
-    echo "Previous line: " . ($e->getPrevious() ? $e->getPrevious()->getLine() : 'none') . "<br>";
-}
+$response = $kernel->handle($request);
+
+$response->send();
+
+$kernel->terminate($request, $response);
